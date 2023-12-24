8b42614015056562cda883dd65f19dd2fabb7db0
Sajjad Haider <sani723@hotmail.com>
2023-12-24 14:25:06 +0400

    Configure CORS option in server

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const axios = require('axios');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 3002;
 
 axios.defaults.headers.common['Authorization'] = `Bearer ${process.env.OPENAI_API_KEY}`;
 
 app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:3000/', // Replace with the domain of your frontend app
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 app.get("/", (req, res) => {
     res.send("Virtual Travel Guide Server is running!");
