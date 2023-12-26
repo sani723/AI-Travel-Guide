bcdae5e0ac7a92b0f3e85cd5c9a2645aa2b8b504
Sajjad Haider <sani723@hotmail.com>
2023-12-26 22:17:03 +0400

    Add the Google Map API key in the env file

--- front-end/src/components/Map/index.js
+++ front-end/src/components/Map/index.js
@@ -3,9 +3,10 @@ import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
 const AiMap = () => {
 
     const position = {lat: 25.2048, lng: 55.2708};
+    
 
     return (
-        <APIProvider apiKey={'AIzaSyCu3_VLJPpmi9qYvQT9S-gLLCBfF16Lhto'}>
+        <APIProvider apiKey={process.env.REACT_APP_MAP_API_KEY}>
             <Map center={position} zoom={10}>
                 <Marker position={position} />
             </Map>
