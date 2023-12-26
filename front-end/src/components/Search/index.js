a1f8be2881eb71fd4daf6d0cbf2300a853007455
Sajjad Haider <sani723@hotmail.com>
2023-12-26 20:50:20 +0400

    Add logic to split the response to get the coordinates from the prompt response

--- front-end/src/components/Search/index.js
+++ front-end/src/components/Search/index.js
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+import axios from 'axios';
 import "./index.css";
 
 const Search = () => {
+
+    const [query, setQuery] = useState('');
+    const [response, setResponse] = useState('');
+
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          const result = await axios.post('http://localhost:3002/dest-coordinates', { query });
+          setResponse(result.data);
+          const [lat, long] = result.data.replace("[","").replace("]","").split(",");
+          
+        } catch (error) {
+          console.error('Error fetching coordinates:', error);
+          setResponse('Error fetching response. Please try again.');
+        }
+      };
+
     return (
         <div className="search-wrapper">
-            <form>
-                <input type="text" placeholder="Enter Destination" />
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={query} onChange={handleInputChange} placeholder="Enter Destination" />
             </form>
         </div>
     );
