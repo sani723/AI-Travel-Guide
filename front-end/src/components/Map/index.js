0bdbc5da09afcb99076533293019522aa1b48b81
Sajjad Haider <sani723@hotmail.com>
2023-12-27 21:51:27 +0400

    Connect the map component with redux store

--- front-end/src/components/Map/index.js
+++ front-end/src/components/Map/index.js
@@ -1,9 +1,72 @@
+import { useEffect, useState } from 'react';
 import {APIProvider, Map, Marker} from '@vis.gl/react-google-maps';
+import { useSelector } from 'react-redux';
 
 const AiMap = () => {
 
-    const position = {lat: 25.2048, lng: 55.2708};
+    //const position = {lat: 25.2048, lng: 55.2708};
+
+    const [position, setPosition] = useState({lat: 25.2048, lng: 55.2708});
+
+    // const coordinates = useSelector((state) => state.todos);
+    //const DestCoordinates = useSelector((state) => state);
     
+
+    // useEffect(() => {
+    //     // Check if DestCoordinates has a value
+    //     if (DestCoordinates) {
+    //       console.log("Coordinates are available:", DestCoordinates.coordinates.queries.data);
+    //     }
+    //   }, [DestCoordinates]); // Dependency array ensures this runs only when DestCoordinates changes
+    
+
+    const coordinatesData = useSelector((state) => {
+        // Find the latest successful query for the 'getCoordinates' endpoint
+        const queries = state.coordinates.queries;
+        const latestQuery = Object.values(queries).reduce((latest, current) => {
+          if (current.endpointName === 'getCoordinates' && current.status === 'fulfilled') {
+            return current.fulfilledTimeStamp > (latest?.fulfilledTimeStamp || 0) ? current : latest;
+          }
+          return latest;
+        }, null);
+    
+        // Return the data from the latest query
+        return latestQuery ? latestQuery.data : null;
+      });
+
+      useEffect(() => {
+
+        let parsedCoordinates;
+      if (coordinatesData) {
+        try {
+          parsedCoordinates = JSON.parse(coordinatesData);
+
+          setPosition({
+            lat: parsedCoordinates[0], lng: parsedCoordinates[1]
+          });
+
+        } catch (error) {
+          console.error("Error parsing coordinates data:", error);
+        }
+      }
+
+      }, [coordinatesData])
+    
+      
+    //   let parsedCoordinates;
+    //   if (coordinatesData) {
+    //     try {
+    //       parsedCoordinates = JSON.parse(coordinatesData);
+
+    //       setPosition({
+    //         lat: parsedCoordinates[0], lng: parsedCoordinates[1]
+    //       });
+
+    //     } catch (error) {
+    //       console.error("Error parsing coordinates data:", error);
+    //     }
+    //   }
+
 
     return (
         <APIProvider apiKey={process.env.REACT_APP_MAP_API_KEY}>
