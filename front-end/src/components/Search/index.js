fda543faeac1c53e6701497d2325917475d1e6be
Sajjad Haider <sani723@hotmail.com>
2023-12-27 21:50:43 +0400

    Connect the search input to the redux

--- front-end/src/components/Search/index.js
+++ front-end/src/components/Search/index.js
@@ -2,33 +2,45 @@ import { useState } from 'react';
 import axios from 'axios';
 import "./index.css";
 
+import { useLazyGetCoordinatesQuery } from "../../services/coordinates";
+
+
 const Search = () => {
 
     const [query, setQuery] = useState('');
+    const [locationName, setLocationName] = useState('');
     const [response, setResponse] = useState('');
 
+    const [ trigger, {data, error, isLoading, isSuccess }] = useLazyGetCoordinatesQuery(locationName);
+
     const handleInputChange = (event) => {
-        setQuery(event.target.value);
+        //setQuery(event.target.value);
+        setLocationName(event.target.value);
     };
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        try {
-          const result = await axios.post('http://localhost:3002/dest-coordinates', { query });
-          setResponse(result.data);
-          const [lat, long] = result.data.replace("[","").replace("]","").split(",");
-          
-        } catch (error) {
-          console.error('Error fetching coordinates:', error);
-          setResponse('Error fetching response. Please try again.');
-        }
+       
+          // const result = await axios.post('http://localhost:3002/dest-coordinates', { query });
+          // setResponse(result.data);
+          // const [lat, long] = result.data.replace("[","").replace("]","").split(",");
+
+          trigger(locationName); 
+          //const [latitude, longitude] = data.replace("[","").replace("]","").split(",");
+
+    
       };
 
     return (
         <div className="search-wrapper">
             <form onSubmit={handleSubmit}>
-                <input type="text" value={query} onChange={handleInputChange} placeholder="Enter Destination" />
+                <input type="text" value={locationName} onChange={handleInputChange} placeholder="Enter Destination" />
             </form>
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+            {/* {data && (
+                <p>Coordinates: Latitude {latitude}, Longitude {longitude}</p>
+            )} */}
         </div>
     );
 }
