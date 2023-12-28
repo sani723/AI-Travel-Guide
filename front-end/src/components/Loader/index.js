a89b57390b9113e78ebfdd4e7a5b8b23497e5c13
Sajjad Haider <sani723@hotmail.com>
2023-12-28 18:34:06 +0400

    Setup the animation configs

--- front-end/src/components/Loader/index.js
+++ front-end/src/components/Loader/index.js
@@ -1,9 +1,20 @@
 import { useRef, useEffect } from "react";
 import lottie from "lottie-web";
+import { current } from "@reduxjs/toolkit";
 
 const Loader = () => {
 
     const container = useRef(null);
+
+    useEffect(() => {
+        lottie.loadAnimation({
+            animationData: require('./loading.json'),
+            autoplay: true,
+            container: container.current,
+            loop: true,
+            renderer: "svg"
+        });
+    }, []);
 
     return (
         <div ref={container} id="loading-container"></div>
